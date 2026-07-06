2973cede091419cf84e20fc82e0d7f9262d3137a
KanakSaini123 <kanak.saini99@gmail.com>
2026-07-07 00:30:09 +0530

    Adding Navbar Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import "./App.css";
 import MovieCard from "./components/MovieCard";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
+import NavBar from "./components/NavBar";
 function App() {
   const movieNumber = 1;
   return (
+    <div>
+      <NavBar/>
     <main className="main-content">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/favorites" element={<Favorites />} />
       </Routes>
     </main>
+    </div>
   );
 }
 
