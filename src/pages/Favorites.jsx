26de91efb2b65851fa3a6a58f89b3418871205fb
KanakSaini123 <kanak.saini99@gmail.com>
2026-07-12 13:33:51 +0530

    added favorites functionality

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,10 +1,35 @@
 import "../css/Favorites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
 
+function Favorites() {
+  const { favorites } = useMovieContext();
 
-function Favorites(){
-    return <div className="favorites-empty">
-        <h2>No Favorite Movies Yet</h2>
-        <p> Start adding movies to your favorite and they will apear here</p>
+  if (favorites) {
+    return (
+      <div className="favorites">
+        <h2>Your Favorite Movies</h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
         </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="favorites">
+        <h2>No Favorites Added!</h2>
+      </div>
+    );
+  }
+
+  return (
+    <div className="favorites-empty">
+      <h2>No Favorite Movies Yet</h2>
+      <p>Start adding movies to your favorites and they will appear here!</p>
+    </div>
+  );
 }
-export default Favorites
+
+export default Favorites;
